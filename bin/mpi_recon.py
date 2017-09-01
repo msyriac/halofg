e5c41ed6b721f2066cd468340f7ad52d19d7a20d
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-09-01 00:26:16 -0400

    resample fft downsampling; no pixel window func required

--- bin/mpi_recon.py
+++ bin/mpi_recon.py
@@ -11,7 +11,7 @@ logger = logging.getLogger()
 with io.nostdout():
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        from enlib import enmap, lensing
+        from enlib import enmap, lensing, resample
 from alhazen.quadraticEstimator import Estimator
 import alhazen.lensTools as lt
 from ConfigParser import SafeConfigParser 
@@ -21,11 +21,12 @@ import argparse
 from mpi4py import MPI
 
 # Runtime params that should be moved to command line
-analysis_section = "analysis"
+#analysis_section = "analysis"
+analysis_section = "analysis_arc"
 sim_section = "sims"
 expf_name = "experiment_simple"
 
-cluster = False
+cluster = True
 simulated_cmb = True
 simulated_kappa = True
 periodic = True
@@ -175,6 +176,7 @@ if rank==0:
 
 
 pixratio = Config.getfloat(analysis_section,"pixel_arcmin")/Config.getfloat(sim_section,"pixel_arcmin")
+px_dat = Config.getfloat(analysis_section,"pixel_arcmin")
 if simulated_cmb or simulated_kappa:
     lens_order = Config.getint(sim_section,"lens_order")
     parray_sim = aio.patch_array_from_config(Config,expf_name,shape_sim,wcs_sim,dimensionless=True)
@@ -223,40 +225,50 @@ for index,kappa_file,cmb_file in zip(my_tasks,my_kappa_files,my_cmb_files):
                 kappa = parray_sim.get_kappa(ktype="grf",vary=False)
 
             phi, fphi = lt.kappa_to_phi(kappa,parray_sim.modlmap,return_fphi=True)
-            alpha_pix = enmap.grad_pixf(fphi)
+            #alpha_pix = enmap.grad_pixf(fphi)
+            grad_phi = enmap.grad(phi)
             
 
     if not(simulated_cmb):
         cmb = np.load(cmb_file) / 1.072480e+09
     else:
+        if rank==0: print "Generating unlensed CMB..."
         unlensed = parray_sim.get_unlensed_cmb(seed=index)
         if random and simulated_kappa:
             lensed = unlensed
         else:
-            lensed = lensing.lens_map_flat_pix(unlensed.copy(), alpha_pix.copy(),order=lens_order)
-        cmb = enmap.downgrade(lensed,pixratio)
+            if rank==0: print "Lensing..."
+            #lensed = lensing.lens_map_flat_pix(unlensed.copy(), alpha_pix.copy(),order=lens_order)
+            lensed = lensing.lens_map(unlensed.copy(), grad_phi, order=lens_order, mode="spline", border="cyclic", trans=False, deriv=False, h=1e-7)
+        if rank==0: print "Downsampling..."
+        cmb = enmap.ndmap(resample.resample_fft(lensed,shape_dat),wcs_dat)
+        #cmb = enmap.downgrade(lensed,pixratio)
         #if rank==0 and k==0: aio.plot_powers(enmap.downgrade(unlensed,pixratio),cmb,parray_dat.modlmap,theory,lbinner_dat,out_dir)
-        hutt2d = fmaps.get_simple_power_enmap(unlensed)
-        hltt2d = fmaps.get_simple_power_enmap(lensed)
-        utt2d = fmaps.get_simple_power_enmap(enmap.downgrade(unlensed,pixratio))
-        ltt2d = fmaps.get_simple_power_enmap(cmb)
-        ccents,utt = lbinner_dat.bin(utt2d)
-        ccents,ltt = lbinner_dat.bin(ltt2d)
-        ccents,hutt = lbinner_sim.bin(hutt2d)
-        ccents,hltt = lbinner_sim.bin(hltt2d)
-        mpibox.add_to_stats("ucl",utt)
-        mpibox.add_to_stats("lcl",ltt)
-        mpibox.add_to_stats("hucl",hutt)
-        mpibox.add_to_stats("hlcl",hltt)
+        if not(cluster):
+            if rank==0: print "Calculating powers for diagnostics..."
+            pxwindow =  fmaps.pixel_window_function(modlmap_dat,angmap_dat,px_dat,px_dat)
+            hutt2d = fmaps.get_simple_power_enmap(unlensed)
+            hltt2d = fmaps.get_simple_power_enmap(lensed)
+            utt2d = fmaps.get_simple_power_enmap(enmap.ndmap(resample.resample_fft(unlensed,shape_dat),wcs_dat))
+            #utt2d = fmaps.get_simple_power_enmap(enmap.downgrade(unlensed,pixratio))/pxwindow**2.
+            ltt2d = fmaps.get_simple_power_enmap(cmb) #/pxwindow**2.
+            ccents,utt = lbinner_dat.bin(utt2d)
+            ccents,ltt = lbinner_dat.bin(ltt2d)
+            ccents,hutt = lbinner_sim.bin(hutt2d)
+            ccents,hltt = lbinner_sim.bin(hltt2d)
+            mpibox.add_to_stats("ucl",utt)
+            mpibox.add_to_stats("lcl",ltt)
+            mpibox.add_to_stats("hucl",hutt)
+            mpibox.add_to_stats("hlcl",hltt)
                 
 
-
+    if rank==0: print "Filtering and binning input kappa..."
     kappa = enmap.ndmap(fmaps.filter_map(kappa,kappa*0.+1.,parray_sim.modlmap,lowPass=kellmax,highPass=kellmin),wcs_sim)
     cents,kappa1d = binner_sim.bin(kappa)
     mpibox.add_to_stats("input_kappa1d",kappa1d)
     mpibox.add_to_stack("input_kappa2d",kappa)
 
-
+    if rank==0: print "Reconstructing..."
     measured = cmb * taper
     fkmaps = fftfast.fft(measured,axes=[-2,-1])
     qest.updateTEB_X(fkmaps,alreadyFTed=True)
@@ -265,25 +277,29 @@ for index,kappa_file,cmb_file in zip(my_tasks,my_kappa_files,my_cmb_files):
         rawkappa = qest.getKappa("TT").real
 
     tapnorm = taper**2. if cluster else 1.
-    kappa_recon = enmap.ndmap(rawkappa/tapnorm-meanfield,wcs_dat)
+    kappa_recon = enmap.ndmap(np.nan_to_num(rawkappa/tapnorm)-meanfield,wcs_dat)
     if cluster: kappa_recon[parray_dat.modrmap*180.*60./np.pi>40.] = 0.
     #kappa_recon = fmaps.filter_map(kappa_recon,kappa_recon*0.+1.,parray_dat.modlmap,lowPass=kellmax,highPass=kellmin)
 
-    downk = enmap.downgrade(enmap.ndmap(kappa,wcs_sim),pixratio) 
-    cpower = fmaps.get_simple_power_enmap(enmap1=kappa_recon,enmap2=downk)/w2
-    apower = fmaps.get_simple_power_enmap(enmap1=kappa_recon)/w4
-    ipower = fmaps.get_simple_power_enmap(enmap1=downk)
-    lcents, cclkk = lbinner_dat.bin(cpower)
-    lcents, aclkk = lbinner_dat.bin(apower)
-    lcents, iclkk = lbinner_dat.bin(ipower)
-
-    
-    cents,kapparecon1d = binner_dat.bin(kappa_recon)
-    mpibox.add_to_stats("recon_kappa1d",kapparecon1d)
-    mpibox.add_to_stats("cross",cclkk)
-    mpibox.add_to_stats("auto",aclkk)
-    mpibox.add_to_stats("ipower",iclkk)
     mpibox.add_to_stack("recon_kappa2d",kappa_recon)
+    if not(cluster):
+        if rank==0: print "Downsampling input kappa..."
+        downk = enmap.ndmap(resample.resample_fft(kappa,shape_dat),wcs_dat)  #enmap.downgrade(enmap.ndmap(kappa,wcs_sim),pixratio)
+        if rank==0: print "Calculating kappa powers and binning..."
+        cpower = fmaps.get_simple_power_enmap(enmap1=kappa_recon,enmap2=downk)/w2
+        apower = fmaps.get_simple_power_enmap(enmap1=kappa_recon)/w4
+        ipower = fmaps.get_simple_power_enmap(enmap1=downk)
+        lcents, cclkk = lbinner_dat.bin(cpower)
+        lcents, aclkk = lbinner_dat.bin(apower)
+        lcents, iclkk = lbinner_dat.bin(ipower)
+
+
+        mpibox.add_to_stats("cross",cclkk)
+        mpibox.add_to_stats("auto",aclkk)
+        mpibox.add_to_stats("ipower",iclkk)
+    else:
+        cents,kapparecon1d = binner_dat.bin(kappa_recon)
+        mpibox.add_to_stats("recon_kappa1d",kapparecon1d)
 
     if rank==0 and index==0:
         io.quickPlot2d(cmb,out_dir+"cmb.png")
@@ -297,14 +313,16 @@ mpibox.get_stats()
 
 
 if rank==0:
-    kappa_stats = mpibox.stats["input_kappa1d"]
-    kapparecon_stats = mpibox.stats["recon_kappa1d"]
 
-    pl = io.Plotter()
-    sgn = 1 if simulated_cmb else -1
-    pl.addErr(cents,kappa_stats['mean'],yerr=kappa_stats['errmean'],ls="-")
-    pl.addErr(cents,sgn*kapparecon_stats['mean'],yerr=kapparecon_stats['errmean'],ls="--")
-    pl.done(out_dir+"kappa1d.png")
+    if cluster:
+        kappa_stats = mpibox.stats["input_kappa1d"]
+        kapparecon_stats = mpibox.stats["recon_kappa1d"]
+
+        pl = io.Plotter()
+        sgn = 1 if simulated_cmb else -1
+        pl.addErr(cents,kappa_stats['mean'],yerr=kappa_stats['errmean'],ls="-")
+        pl.addErr(cents,sgn*kapparecon_stats['mean'],yerr=kapparecon_stats['errmean'],ls="--")
+        pl.done(out_dir+"kappa1d.png")
 
     if not(random):
         filename = "/profiles_simcmb_"+str(simulated_cmb)+"_simkap_"+str(simulated_kappa)+"_periodic_"+str(periodic)+".txt"
@@ -318,92 +336,92 @@ if rank==0:
     reconstack = mpibox.stacks["recon_kappa2d"]
     io.quickPlot2d(inpstack,out_dir+"inpstack.png")
     io.quickPlot2d(reconstack,out_dir+"reconstack.png")
-    inp = enmap.downgrade(enmap.ndmap(inpstack,wcs_sim),pixratio) 
+    inp = enmap.ndmap(resample.resample_fft(inpstack,shape_dat),wcs_dat) # enmap.downgrade(enmap.ndmap(inpstack,wcs_sim),pixratio) 
     pdiff = np.nan_to_num((inp-reconstack)*100./inp)
     io.quickPlot2d(pdiff,out_dir+"pdiffstack.png",lim=20.)
     np.save(save_dir+"/reconstack",reconstack)
 
 
-    cstats = mpibox.stats['cross']
-    istats = mpibox.stats['ipower']
-    astats = mpibox.stats['auto']
-    pl = io.Plotter(scaleY='log')
-    pl.addErr(lcents,cstats['mean'],yerr=cstats['errmean'],marker="o")
-    pl.add(lcents,istats['mean'],marker="x",ls="none")
-    lcents,nlkk = lbinner_dat.bin(qest.N.Nlkk['TT'])
-    ellrange = np.arange(2,kellmax,1)
-    clkk = theory.gCl("kk",ellrange)
-    pl.addErr(lcents,astats['mean'],yerr=astats['errmean'],marker="o",alpha=0.5)
-    pl.add(lcents,nlkk,ls="--")
-    pl.add(ellrange,clkk,color="k")
-    pl.done(out_dir+"cpower.png")
+    if not(cluster):
+        cstats = mpibox.stats['cross']
+        istats = mpibox.stats['ipower']
+        astats = mpibox.stats['auto']
+        pl = io.Plotter(scaleY='log')
+        pl.addErr(lcents,cstats['mean'],yerr=cstats['errmean'],marker="o")
+        pl.add(lcents,istats['mean'],marker="x",ls="none")
+        lcents,nlkk = lbinner_dat.bin(qest.N.Nlkk['TT'])
+        ellrange = np.arange(2,kellmax,1)
+        clkk = theory.gCl("kk",ellrange)
+        pl.addErr(lcents,astats['mean'],yerr=astats['errmean'],marker="o",alpha=0.5)
+        pl.add(lcents,nlkk,ls="--")
+        pl.add(ellrange,clkk,color="k")
+        pl.done(out_dir+"cpower.png")
 
-    pl = io.Plotter()
-    ldiff = (cstats['mean']-istats['mean'])*100./istats['mean']
-    lerr = cstats['errmean']*100./istats['mean']
-    pl.addErr(lcents,ldiff,yerr=lerr,marker="o",ls="-")
-    pl._ax.axhline(y=0.,ls="--",color="k")
-    pl.done(out_dir+"powerdiff.png")
-
-
-    
-    
-    iutt2d = theory.uCl("TT",parray_dat.modlmap)
-    iltt2d = theory.lCl("TT",parray_dat.modlmap)
-    ccents,iutt = lbinner_dat.bin(iutt2d)
-    ccents,iltt = lbinner_dat.bin(iltt2d)
-    uclstats = mpibox.stats["ucl"]
-    lclstats = mpibox.stats["lcl"]
-    huclstats = mpibox.stats["hucl"]
-    hlclstats = mpibox.stats["hlcl"]
-
-    utt = uclstats['mean']
-    ltt = lclstats['mean']
-    utterr = uclstats['errmean']
-    ltterr = lclstats['errmean']
-    
-    hutt = huclstats['mean']
-    hltt = hlclstats['mean']
-    hutterr = huclstats['errmean']
-    hltterr = hlclstats['errmean']
-
-    
-    pl = io.Plotter()
-
-    pdiff = (hutt-iutt)*100./iutt
-    perr = 100.*hutterr/iutt
-
-    pl.addErr(ccents,pdiff,yerr=perr,marker="x",ls="none",label="unlensed highres",alpha=0.5)
-
-    pdiff = (hltt-iltt)*100./iltt
-    perr = 100.*hltterr/iltt
-
-    pl.addErr(ccents+5,pdiff,yerr=perr,marker="o",ls="none",label="lensed highres",alpha=0.5)
+        pl = io.Plotter()
+        ldiff = (cstats['mean']-istats['mean'])*100./istats['mean']
+        lerr = cstats['errmean']*100./istats['mean']
+        pl.addErr(lcents,ldiff,yerr=lerr,marker="o",ls="-")
+        pl._ax.axhline(y=0.,ls="--",color="k")
+        pl.done(out_dir+"powerdiff.png")
 
 
     
-    pdiff = (utt-iutt)*100./iutt
-    perr = 100.*utterr/iutt
+        iutt2d = theory.uCl("TT",parray_dat.modlmap)
+        iltt2d = theory.lCl("TT",parray_dat.modlmap)
+        ccents,iutt = lbinner_dat.bin(iutt2d)
+        ccents,iltt = lbinner_dat.bin(iltt2d)
+        uclstats = mpibox.stats["ucl"]
+        lclstats = mpibox.stats["lcl"]
+        huclstats = mpibox.stats["hucl"]
+        hlclstats = mpibox.stats["hlcl"]
 
-    pl.addErr(ccents,pdiff,yerr=perr,marker="x",ls="none",label="unlensed")
+        utt = uclstats['mean']
+        ltt = lclstats['mean']
+        utterr = uclstats['errmean']
+        ltterr = lclstats['errmean']
 
-    pdiff = (ltt-iltt)*100./iltt
-    perr = 100.*ltterr/iltt
-
-    pl.addErr(ccents+5,pdiff,yerr=perr,marker="o",ls="none",label="lensed")
-    pl.legendOn(labsize=10)
-    pl._ax.axhline(y=0.,ls="--",color="k")
-    pl.done(out_dir+"clttpdiff.png")
+        hutt = huclstats['mean']
+        hltt = hlclstats['mean']
+        hutterr = huclstats['errmean']
+        hltterr = hlclstats['errmean']
 
 
-        
-    pl = io.Plotter(scaleY='log',scaleX='log')
+        pl = io.Plotter()
 
-    pl.add(ccents,iutt*ccents**2.)
-    pl.addErr(ccents,utt*ccents**2.,yerr=utterr*ccents**2.,marker="x",ls="none",label="unlensed")
+        pdiff = (hutt-iutt)*100./iutt
+        perr = 100.*hutterr/iutt
 
-    pl.add(ccents,iltt*ccents**2.)
-    pl.addErr(ccents,ltt*ccents**2.,yerr=ltterr*ccents**2.,marker="o",ls="none",label="lensed")
+        pl.addErr(ccents-50,pdiff,yerr=perr,marker="x",ls="none",label="unlensed highres",alpha=0.5)
 
-    pl.legendOn(labsize=10)
-    pl.done(out_dir+"clttp.png")
+        pdiff = (hltt-iltt)*100./iltt
+        perr = 100.*hltterr/iltt
+
+        pl.addErr(ccents-25,pdiff,yerr=perr,marker="o",ls="none",label="lensed highres",alpha=0.5)
+
+
+
+        pdiff = (utt-iutt)*100./iutt
+        perr = 100.*utterr/iutt
+
+        pl.addErr(ccents+25,pdiff,yerr=perr,marker="x",ls="none",label="unlensed")
+
+        pdiff = (ltt-iltt)*100./iltt
+        perr = 100.*ltterr/iltt
+
+        pl.addErr(ccents+50,pdiff,yerr=perr,marker="o",ls="none",label="lensed")
+        pl.legendOn(labsize=10,loc="lower left")
+        pl._ax.axhline(y=0.,ls="--",color="k")
+        pl.done(out_dir+"clttpdiff.png")
+
+
+
+        pl = io.Plotter(scaleY='log',scaleX='log')
+
+        pl.add(ccents,iutt*ccents**2.)
+        pl.addErr(ccents,utt*ccents**2.,yerr=utterr*ccents**2.,marker="x",ls="none",label="unlensed")
+
+        pl.add(ccents,iltt*ccents**2.)
+        pl.addErr(ccents,ltt*ccents**2.,yerr=ltterr*ccents**2.,marker="o",ls="none",label="lensed")
+
+        pl.legendOn(labsize=10)
+        pl.done(out_dir+"clttp.png")
