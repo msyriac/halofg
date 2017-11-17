ebd01b55267dd0f683cf61666882e8380a8c0e9c
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-11-17 00:20:44 -0500

    animation

--- halofg/sehgalInterface.py
+++ halofg/sehgalInterface.py
@@ -21,7 +21,7 @@ def get_component_map_from_config(PathConfig,SimConfig,section,base_nside=None):
     units = Config.getfloat(sec_name,"unit_conversion")
     return hpmap*calib*units
 
-def get_components_map_from_config(SimConfig,freq,components,base_nside=None):
+def get_components_map_from_config(PathConfig,SimConfig,freq,components,base_nside=None):
     """
     Returns sum of healpix maps from Sehgal et. al. / Hill sims
     in the same units with the right calibration
@@ -32,10 +32,9 @@ def get_components_map_from_config(SimConfig,freq,components,base_nside=None):
     totmap = 0.
     for component in components:
         sec_name = str(int(freq))+"_"+component
-        retmap = get_component_map_from_config(SimConfig,sec_name,base_nside=base_nside)
+        retmap = get_component_map_from_config(PathConfig,SimConfig,sec_name,base_nside=base_nside)
         totmap = totmap + retmap
 
-    del hpmap
     return totmap
     
 
@@ -56,22 +55,90 @@ def get_kappa(PathConfig,SimConfig,section="kappa",base_nside=None):
 
 
 
-def select_from_halo_catalog(PathConfig,SimConfig,catalog_section='catalog_default',M200_min=-np.inf,M200_max=np.inf,z_min=-np.inf,z_max=np.inf,Nmax=None,random_sampling=True,histogram_z_save_path=None,histogram_M_save_path=None):
+def select_from_halo_catalog(PathConfig,SimConfig,catalog_section='catalog_default',M200_min=-np.inf,M200_max=np.inf,z_min=-np.inf,z_max=np.inf,Nmax=None,random_sampling=True,histogram_z_save_path=None,histogram_M_save_path=None,ra_min=None,ra_max=None,dec_min=None,dec_max=None,mirror=True):
     import pandas as pd
     Config = SimConfig
     map_root = PathConfig.get("paths","input_data")
 
     halo_file = map_root + Config.get(catalog_section,'file')
     df = pd.read_hdf(halo_file)
+
+    ra = df['RA']*np.pi/180.
+    dec = df['DEC']*np.pi/180.
+
+    if mirror:
+        new_dfs = []
+
+        for n in range(0,4):
+            new_df = df.copy()
+            new_df['RA'] = (ra+n*np.pi/2.)*180./np.pi
+            new_dfs.append(new_df.copy())
+
+        for n in range(4):
+            new_df = df.copy()
+            new_df['RA'] = (np.pi/2.-ra+n*np.pi/2.)*180./np.pi
+            new_df['DEC'] = -dec*180./np.pi
+            new_dfs.append(new_df.copy())
+
+        final_df = pd.concat(new_dfs)
+        assert final_df['RA'].size==8*df['RA'].size
+    else:
+        final_df = df
+
+
+    if ra_min is not None: assert ra_min<ra_max
+    if ra_max is not None: assert dec_min<dec_max
+
+    if ra_min is None:
+        cond = np.abs(final_df['RA'])>=0.
+    else:
+        ra_left = 360.+ra_min if ra_min<0. else ra_min
+        ra_right = 360.+ra_max if ra_max<0. else ra_max
+
+        if ra_min*ra_max<0.:
+            cond = (final_df['RA']>ra_left) | (final_df['RA']<ra_right)
+        else:
+            cond = (final_df['RA']>ra_left) & (final_df['RA']<ra_right)
+
+    if dec_min is None:
+        cond2 = np.abs(final_df['DEC'])>=0.
+    else:
+        cond2 = (final_df['DEC']>dec_min) & (final_df['DEC']<dec_max)
+        
+    sel = cond2 & cond
+    final_df =final_df[sel]
+
+    
+    return select_from_halo_catalog_dataframe(final_df,M200_min,M200_max,z_min,z_max,Nmax,random_sampling,histogram_z_save_path,histogram_M_save_path)
+    
+
+def select_from_halo_catalog_dataframe(df_in,M200_min=-np.inf,M200_max=np.inf,z_min=-np.inf,z_max=np.inf,Nmax=None,random_sampling=True,histogram_z_save_path=None,histogram_M_save_path=None):
+
+    df = df_in.copy()
+    
     sel = (df['Z']>z_min) & (df['Z']<z_max) & (df['M200']>M200_min) & (df['M200']<M200_max)
     assert random_sampling, "NotImplementedError: non-random sampling not implemented."
-    df = df[sel].sample(Nmax) if ((Nmax is not None) and Nmax<df['Z'].size) else df[sel]
+
+    try:
+        df = df[sel].sample(Nmax,replace=False) if ((Nmax is not None) and Nmax<df['Z'].size) else df[sel]
+    except:
+        print Nmax
+        print df['Z'].size
+        print df[sel]['Z'].size
+        print M200_min,M200_max
+        print z_min,z_max
+        print "ERROR: NOT ENOUGH TO SAMPLE FROM"
+        sys.exit()
 
     
     halos_select_z = df['Z']
     halos_select_M200 = df['M200']
     halos_select_RA = df['RA']
     halos_select_DEC = df['DEC']
+
+
+
+
 
 
     if histogram_z_save_path is not None:
---
+++ tests/octants.py
@@ -0,0 +1,81 @@
+import healpy as hp
+import halofg.sehgalInterface as si
+import orphics.tools.catalogs as cats
+import orphics.io as io
+import numpy as np
+import astropy.coordinates
+import sys
+
+
+# patches: y0,x0,y1,x1 (y=DEC deg, x=RA deg)
+deep56_box =  [-7.5,38.5,4.,-5.5]
+y0,x0,y1,x1 = deep56_box
+hwidth = x0-x1
+vwidth = y1-y0
+
+def healpix_box_indices(nside,y0,x0,y1,x1):
+
+    ra_left = x0
+    ra_right = x1
+    dec_down = y0
+    dec_up = y1
+    
+    dec_poly, ra_poly = (np.array([ dec_down,  dec_down,  dec_up,  dec_up]), np.array([ ra_left,  ra_right,  ra_right,  ra_left]))
+    xyzpoly = astropy.coordinates.spherical_to_cartesian(1, np.deg2rad(dec_poly), np.deg2rad(ra_poly))
+    return hp.query_polygon(nside,np.array(xyzpoly).T)
+
+nside = 128
+pix = healpix_box_indices(nside,*deep56_box)
+tmap = np.zeros(hp.nside2npix(nside))
+tmap[pix] = 1
+# io.mollview(tmap,io.dout_dir+"tmap.png")#,coord=['G','C'])
+# sys.exit()
+
+SimConfig = io.config_from_file("../halofg/input/sehgal.ini")
+PathConfig = io.config_from_file("../halofg/input/paths_local.ini")
+
+
+
+df,ra,dec,m200_sehgal,z_sehgal = si.select_from_halo_catalog(PathConfig,SimConfig,catalog_section='catalog_m200mean',M200_min=0.,M200_max=np.inf,z_min=0,z_max=np.inf,Nmax=None,random_sampling=True,histogram_z_save_path=None,histogram_M_save_path=None,mirror=False)
+
+hcat = cats.HealpixCatMapper(nside,ra,dec)
+
+frames = 5
+phi = 0.
+thetas = np.linspace(0,30,frames)
+psi = 0.
+
+
+hmap = hcat.counts
+print "Rotating..."
+
+
+
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+fig.set_tight_layout(True)
+
+# animation function
+def animate(i):
+    theta = np.asarray(thetas[i])
+    alm=hp.map2alm(hmap)
+    hp.rotate_alm(alm,psi*np.pi/180.,theta*np.pi/180.,phi*np.pi/180.)
+    hmap_new=hp.alm2map(alm,nside=nside)
+    pltimage = io.mollview(tmap*300+hmap_new,return_projected_map=True)
+    cont = ax.imshow(np.flipud(pltimage))
+    print phi
+    return cont  
+
+anim = animation.FuncAnimation(fig, animate, frames=np.arange(0,frames),interval=400)
+ 
+anim.save(io.dout_dir+'hcat.gif', dpi=80, writer='imagemagick')
+
+
+
+# psi1, theta1, phi1 = hp.rotator.coordsys2euler_zyz(['C','G'])
+# print psi1,theta1,phi1
+# hp.rotate_alm(alm,psi1,theta1,phi1)
+# psi2, theta2, phi2 = hp.rotator.coordsys2euler_zyz(['G','C'])
+# hp.rotate_alm(alm,psi2,theta2,phi2)
