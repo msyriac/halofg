570939ea2a8f0d4081370d36ea1e8645a237e948
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-11-24 23:47:48 -0500

    power ilc

--- bin/sz_gauss.py
+++ bin/sz_gauss.py
@@ -4,6 +4,8 @@ from orphics import io,maps,stats,cosmology
 import numpy as np, os, sys
 from enlib import enmap,fft
 
+
+
 const = cosmology.defaultConstants
 theory_file_root = "data/Aug6_highAcc_CDM"
 theory = cosmology.loadTheorySpectraFromCAMB(theory_file_root,unlensedEqualsLensed=False,
@@ -19,7 +21,8 @@ shape,wcs = maps.rect_geometry(width_deg=10.,px_res_arcmin=2.0)
 modlmap = enmap.modlmap(shape,wcs)
 lmax = np.max(modlmap)
 ells = np.arange(0,lmax,1)
-ps = theory.lCl('TT',ells).reshape((1,1,ells.size))
+cmb_ps_1d = theory.lCl('TT',ells) 
+ps = cmb_ps_1d.reshape((1,1,ells.size))
 mg = maps.MapGen(shape,wcs,ps)
 
 TCMB = 2.27255e6
@@ -30,12 +33,15 @@ N = 100
 # freqs = np.array([90.,148.,220.,353.])
 # noises = np.array([1.,1.,1.,1.])/100.
 freqs = np.array([90.,148.])
-noises = np.array([1.,1.])/100.
+f_nus = f_nu(const,freqs)
+noises = np.array([2.,1.])/100.*100.
 beam = 1.4
 beams = beam *148./freqs
 kbeams = []
+kbeams1d = []
 for beam in beams:
     kbeams.append(maps.gauss_beam(beam,modlmap))
+    kbeams1d.append(maps.gauss_beam(beam,ells))
 
 
 mg_noises = []
@@ -45,7 +51,7 @@ for noise in noises:
     mg_noises.append( maps.MapGen(shape,wcs,ps_noise))
 
 fc = maps.FourierCalc(shape,wcs)
-bin_edges = np.arange(30,6000,40)
+bin_edges = np.arange(30,6000,80)
 binner = stats.bin2D(modlmap,bin_edges)
 
 
@@ -55,19 +61,22 @@ fgen = fgGenerator(shape,wcs,components,fgs)
 
 
 
-nfreqs = len(noises)
+# nfreqs = len(noises)
 cmb2d = theory.lCl('TT',modlmap)
-Covmat = np.tile(cmb2d,(nfreqs,nfreqs,1,1))#,modlmap.shape[0],modlmap.shape[1]))
+# Covmat = np.tile(cmb2d,(nfreqs,nfreqs,1,1))#,modlmap.shape[0],modlmap.shape[1]))
 
-for i,(kbeam1,freq1,noise1) in enumerate(zip(kbeams,freqs,noises)):
-    for j,(kbeam2,freq2,noise2) in enumerate(zip(kbeams,freqs,noises)):
-        if i==j:
-            Covmat[i,j,:,:] += modlmap*0.+(noise1*np.pi/180./60.)**2./kbeam1**2.
-        for component in components:
-            Covmat[i,j,:,:] += fgen.get_noise(component,freq1,freq2,modlmap)
+# for i,(kbeam1,freq1,noise1) in enumerate(zip(kbeams,freqs,noises)):
+#     for j,(kbeam2,freq2,noise2) in enumerate(zip(kbeams,freqs,noises)):
+#         if i==j:
+#             Covmat[i,j,:,:] += modlmap*0.+(noise1*np.pi/180./60.)**2./kbeam1**2.
+#         for component in components:
+#             Covmat[i,j,:,:] += fgen.get_noise(component,freq1,freq2,modlmap)
 
 
-cinv = np.linalg.inv(Covmat.T).T
+# cinv = np.linalg.inv(Covmat.T).T
+wnoises = (noises*np.pi/180./60.)**2.
+cinv = maps.ilc_cinv(modlmap,cmb2d,kbeams,freqs,wnoises,components,fgen)
+cinv_1d = maps.ilc_cinv(ells,cmb_ps_1d,kbeams1d,freqs,wnoises,components,fgen)
 print(cinv.shape)
 
 kmask = maps.mask_kspace(shape,wcs, lmin = None, lmax = 6000)
@@ -108,36 +117,22 @@ for i in range(N):
 
     kuncleans = np.stack(kuncleans)
 
-    kcleaned = maps.ilc_cmb(kuncleans,cinv)
+    kcleaned = maps.silc(kuncleans,cinv)
+    ckcleaned = maps.cilc(kuncleans,cinv,f_nus*0.+1.,f_nus)
     p2d_cleaned = fc.f2power(kcleaned,kcleaned)
     cents, p1dcleaned = binner.bin(p2d_cleaned)
+    cp2d_cleaned = fc.f2power(ckcleaned,ckcleaned)
+    cents, cp1dcleaned = binner.bin(cp2d_cleaned)
     pl.add(cents,p1dcleaned*cents**2.,ls="none",marker="o")
+    pl.add(cents,cp1dcleaned*cents**2.,ls="none",marker="o")
     pl.add(ells,ps[0,0,:]*ells**2.,color="k",lw=2)
+    pl.add(ells,maps.silc_noise(cinv_1d)*ells**2.,ls="-.",lw=2,label="silc")
+    pl.add(ells,maps.cilc_noise(cinv_1d,f_nus*0.+1.,f_nus)*ells**2.,ls="-.",lw=2,label="cilc sz")
     pl._ax.set_xlim(2,6000)
-    pl.legend()
+    pl._ax.set_ylim(1e0,1e5)
+    pl.legend(loc="lower left")
     pl.done("cls.png")
     io.plot_img(unclean,"unclean.png",lim=[-300,300])
     io.plot_img(fft.ifft(kcleaned,axes=[-2,-1],normalize=True).real,"cleaned.png",lim=[-300,300])
     sys.exit()
 
-
-def make_cinv(cmb2d,kbeams,freqs,noises,const_dict=cosmology.defaultConstants):
-    """
-    cmb2d -- Theory C_ell_TT in 2D fourier space
-
-    """
-
-    nfreqs = len(noises)
-    cmb2d = theory.lCl('TT',modlmap)
-    Covmat = np.tile(cmb2d,(nfreqs,nfreqs,1,1))#,modlmap.shape[0],modlmap.shape[1]))
-
-    for i,(kbeam1,freq1,noise1) in enumerate(zip(kbeams,freqs,noises)):
-        for j,(kbeam2,freq2,noise2) in enumerate(zip(kbeams,freqs,noises)):
-            if i==j:
-                Covmat[i,j,:,:] += modlmap*0.+(noise1*np.pi/180./60.)**2./kbeam1**2.
-            for component in components:
-                Covmat[i,j,:,:] += fgen.get_noise(component,freq1,freq2,modlmap)
-
-
-    cinv = np.linalg.inv(Covmat.T).T
-    print(cinv.shape)
